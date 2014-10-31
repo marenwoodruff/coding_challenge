230c9b5d049ad61bc5a2c2c7826e1558dbd4e6d0
MarenWoodruff <wren403@gmail.com>
2014-10-30 21:58:01 -0400

    add first_name last_name username columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030215038) do
+ActiveRecord::Schema.define(version: 20141031014723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,11 @@ ActiveRecord::Schema.define(version: 20141030215038) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
   end
 
 end
