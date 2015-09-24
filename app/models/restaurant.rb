603fe0af79cb3130f6b085bc6828a2b102b92ab5
MarenWoodruff <wren403@gmail.com>
2015-09-24 12:23:24 -0400

    working on restaurant logic.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,5 @@
 class Restaurant < ActiveRecord::Base
     has_many :reviews, dependent: :destroy
-    has_many :users, through: :reviews
 
     validates :name, presence: true, length: { maximum: 70 }
     validates :address, presence: true, length: { maximum: 300 }
@@ -9,12 +8,12 @@ class Restaurant < ActiveRecord::Base
     def suggestion
         choice_array = Restaurant.all
         suggest = choice_array.sort_by { |x| [x.updated_at, x.reviews] }.shuffle
-        if suggest[0].name == Restaurant.last.updated_at
-            suggest[0].pop
-            suggest = choice_array.sort_by { |x| [x.updated_at, x.reviews] }.shuffle
-        else 
-            suggest = suggest[0].name
-        end    
+        # if suggest[0].name == Restaurant.last.updated_at
+        #     suggest[0].pop
+        #     suggest = choice_array.sort_by { |x| [x.updated_at, x.reviews] }.shuffle
+        # else 
+            suggest = suggest[1]
+        # end    
     end
 end
 
